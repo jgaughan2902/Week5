a0677953672d3f50f8bdb0263e947a1c6e850b03
jgaughan2902 <jdgaugha@iu.edu>
2025-09-26 15:21:56 +0000

    Adding Questions to app.py

--- app.py
+++ app.py
@@ -4,32 +4,21 @@ from apputil import *
 # Load Titanic dataset
 df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
 
-st.write(
-'''
-# Titanic Visualization 1
+# Exercise 1
+st.write("Did younger children have a higher survival rate than older individuals?")
 
-'''
-)
-# Generate and display the figure
 fig1 = visualize_demographic()
 st.plotly_chart(fig1, use_container_width=True)
 
+# Exercise 2
 
+st.write("The output from the last_names() function seems" \
+"to agree with the data table. If we pull up the raw .csv" \
+"and use Ctrl+F and search up a given name, we'll see the" \
+"count of the output match with the output of ctrl+F.")
 
-st.write(
-'''
-# Titanic Visualization 2
-'''
-)
+st.write("Did males or females have a higher average fare?")
 # Generate and display the figure
 fig2 = visualize_families()
 st.plotly_chart(fig2, use_container_width=True)
 
-st.write(
-'''
-# Titanic Visualization Bonus
-'''
-)
-# Generate and display the figure
-fig3 = visualize_family_size()
-st.plotly_chart(fig3, use_container_width=True)
